60fb2eaf8f180ef414b9d2a2babdb04065777e9a
Sam Rapier <samrapier@live.co.uk>
2022-03-30 19:06:34 +0100

    Added Canny Algorithm

---
+++ workspace/canny.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+def non_max_suppression(img, D):
+    width, height = img.shape
+    returnImg = np.zeros((width,height), dtype=np.uint8)
+    angle = D * 180. / np.pi
+    angle[angle < 0] += 180
+
+    
+    for i in range(1,width-1):
+        for j in range(1,height-1):
+            try:
+                q = 255
+                r = 255
+                
+               #angle 0
+                if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                    q = img[i, j+1]
+                    r = img[i, j-1]
+                #angle 45
+                elif (22.5 <= angle[i,j] < 67.5):
+                    q = img[i+1, j-1]
+                    r = img[i-1, j+1]
+                #angle 90
+                elif (67.5 <= angle[i,j] < 112.5):
+                    q = img[i+1, j]
+                    r = img[i-1, j]
+                #angle 135
+                elif (112.5 <= angle[i,j] < 157.5):
+                    q = img[i-1, j-1]
+                    r = img[i+1, j+1]
+
+                if (img[i,j] >= q) and (img[i,j] >= r):
+                    returnImg[i,j] = img[i,j]
+                else:
+                    returnImg[i,j] = 0
+
+            except IndexError as e:
+                pass
+    
+    return returnImg    
+
+
+def threshold(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
+    
+    highThreshold = img.max() * highThresholdRatio
+    lowThreshold = highThreshold * lowThresholdRatio
+    
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.uint8)
+    
+    weak = np.int32(25)
+    strong = np.int32(255)
+    
+    strong_i, strong_j = np.where(img >= highThreshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+    
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+    
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+    
+    return (res, weak, strong)
+
+
+def canny(img, theta):
+    res = non_max_suppression(img, theta)
+    res, weak, strong = threshold(res)
+    return res
